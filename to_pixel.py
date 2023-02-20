f688c12f975e37c5ccc7df32ba072ec34c596000
Estevao <estevaopfon@gmail.com>
2023-02-20 02:36:37 -0300

    wip

--- to_pixel.py
+++ to_pixel.py
@@ -3,10 +3,24 @@ from PIL import Image
 import time
 
 attack_animation_bool = False
+class Pokemon:
+    def __init__(self, name, hp, atk, defense, level=5, hp_max=100, speed=10):
+        self.name = name
+        self.hp = hp
+        self.hp_max = hp_max
+        self.atk = atk
+        self.defense = defense
+        self.level = level
+
+    def attack(self, enemy):
+        enemy.hp -= self.atk
+
+    def __str__(self):
+        return f"{self.name} - HP: {self.hp} - ATK: {self.atk} - DEF: {self.defense} - SPD: {self.speed}"
 def main(stdscr):
     # Load the image file
     #curses.use_default_colors()
-
+    option = 0
     def draw_image(stdscr, img_file, x_pos, y_pos, size_x=50, size_y=20):
         img = Image.open(img_file)
         curses.start_color()
@@ -22,6 +36,7 @@ def main(stdscr):
         curses.init_pair(6, curses.COLOR_CYAN, -1)
         curses.init_pair(7, curses.COLOR_WHITE, -1)
         curses.init_pair(8, curses.COLOR_BLACK, -1)
+        curses.init_pair(9, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
         # Scale the image down to fit the terminal size
         if height > curses.LINES or width > curses.COLS:
@@ -84,6 +99,7 @@ def main(stdscr):
         draw_image(stdscr, "charmander.png", 0, 10)
         stdscr.refresh()
 
+
     def atack_animation_enemy(stdscr):
         draw_image(stdscr, "white.png", 0, 0, size_x=size_x_white, size_y=size_y_white)
         draw_image(stdscr, "bulbinha.png", 60, 0, size_x=30, size_y=20)
@@ -93,8 +109,44 @@ def main(stdscr):
         draw_image(stdscr, "bulbinha.png", 70, 0, size_x=30, size_y=20)
         stdscr.refresh()
 
+    def draw_hp_bar(stdscr, hp, x, y):
+        hp_bar = ""
+        hp_bar += "HP: "
+        hp_bar += str(hp)
+        hp_bar += "/"
+        color = 2
+        stdscr.addstr(y, x, "\u2588"*(hp//10)*2, curses.color_pair(color))
+
+    def draw_status(stdscr, pokemon, x, y):
+        stdscr.addstr(y-1, x-2, pokemon.name.upper(), curses.color_pair(9))
+        draw_hp_bar(stdscr, pokemon.hp, x, y)
+        stdscr.addstr(y-1, x+17, f"Lv{pokemon.level}", curses.color_pair(9))
+        stdscr.addstr(y+1, x+13, f"{pokemon.hp}/{pokemon.hp_max}", curses.color_pair(9))
+
+    def attack_options(stdscr, pokemon, x, y):
+        stdscr.addstr(y+1, x, "TACLKE", curses.color_pair(9))
+        stdscr.addstr(y+1, x+10, "GROWL", curses.color_pair(9))
+        nonlocal option
+
+        if c == curses.KEY_RIGHT:
+            option = 1
+        elif c == curses.KEY_LEFT:
+            option = 0
+
+        if option == 0:
+            stdscr.addstr(y + 1, x - 2, ">", curses.color_pair(9))
+            stdscr.addstr(y + 1, x + 8, " ", curses.color_pair(9))
+        if option == 1:
+            stdscr.addstr(y + 1, x - 2, " ", curses.color_pair(9))
+            stdscr.addstr(y + 1, x + 8, ">", curses.color_pair(9))
+
+
+
     x = 0
     draw_image(stdscr, "white.png", 0, 0, size_x=size_x_white, size_y=size_y_white)
+
+    bulbasaur = Pokemon("Bulbasaur", 100, 10, 15)
+    charmander = Pokemon("Charmander", 100, 11, 9)
     while True:
         idle_animation(stdscr)
         draw_image(stdscr, "bulbinha.png", 70, x, size_x=30, size_y=20)
@@ -110,18 +162,24 @@ def main(stdscr):
             stdscr.clear()
         if c == ord('f'):
             atack_animation(stdscr)
+            charmander.attack(bulbasaur)
         if c == ord('g'):
             atack_animation_enemy(stdscr)
-
-
 
         elif c == ord('q'):
             import sys
             sys.exit(0)
+        if (c == curses.KEY_ENTER or c == 10 or c == 13) and option == 0:
+            atack_animation(stdscr)
+            charmander.attack(bulbasaur)
 
 
-
-
+        draw_hp_bar(stdscr, bulbasaur.hp, 30, 5)
+        #draw_hp_bar(stdscr, charmander.hp, 60, 22)
+        #draw_name(stdscr, bulbasaur.name, 30, 4)
+        draw_status(stdscr, bulbasaur, 30, 5)
+        draw_status(stdscr, charmander, 60, 22)
+        attack_options(stdscr, charmander, 60, 25)
 
 
 if __name__ == "__main__":
